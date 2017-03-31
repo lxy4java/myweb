b2bb6e2c4c366c5d8401b2554107eb87dda1fc7c
lixy <lixiaoyao@zszq.com>
2017-03-31 16:01:11 +0800

    delete

--- src/components/Table.vue
+++ src/components/Table.vue
@@ -2,10 +2,10 @@
   <div class="xyTable">
       <table>
             <tr>
-            <th v-for="tt in title">{{tt}}</th>
+            <th v-for="tt in title">{{tt}}</th><th>Delete</th>
             </tr>
             <tr v-for="dt in data">
-               <td v-for="d in dt">{{d}}</td>
+               <td v-for="d in dt">{{d}}</td><td><button v-on:click="mydel(dt[0])" >Delete</button></td>
             </tr>
       </table>
   </div>
@@ -39,6 +39,19 @@ export default {
             .catch(function (error) {
              console.log(error);
           });
+    },
+    mydel:function(name){
+       this.$http.get("/rest/delete?name="+name)
+            .then((response) => {
+            var data =response.data;
+            console.log(data['title'])
+            this.$set(this,'title', data['title']);
+            this.$set(this,'data', data['data'])
+          })
+            .catch(function (error) {
+             console.log(error);
+          });
+
     }
   }
 
--- src/test.js
+++ src/test.js
@@ -1,22 +1,38 @@
 var express = require('express');
 var app = express();
 
-app.get('/navis', function(req, res) {
+var tabledata = { "title": ["name", "age", "gender"], "data": [["lxy", "28", "male"], ["大公", "666", "male"]] };
+
+app.get('/navis', function (req, res) {
 
   //res.type('application/json');  
   res.send('[{"first": [{ "url": "/hello", "name": "hello" }, { "url": "/", "name": "home" }]}, {"second": [{ "url": "/hello", "name": "hello" }, { "url": "/", "name": "home" }]}, {"third": [{ "url": "/hello", "name": "hello" }, { "url": "/", "name": "home" }]}, {"fourth": []}]');
 });
 
-app.get('/routers', function(req, res) {
+app.get('/routers', function (req, res) {
 
   //res.type('application/json');  
   res.send("[{ path: '/',component: Home}, {path: '/hello',component: Hello}]");
 });
 
-app.get('/table', function(req, res) {
+app.get('/table', function (req, res) {
 
   //res.type('application/json');  
-  res.send('{"title": ["name","age","gender"] , "data": [["lxy","28","male"],["大公","666","male"]]}');
+  res.send(tabledata);
+});
+
+app.get('/delete', function (req, res) {
+  var name = req.query.name;
+  console.log(name);
+  //res.type('application/json');
+  var data = tabledata['data'];
+  for (var i = 0; i < data.length; i++) {
+      if(data[i][0]==name){
+        tabledata['data'].splice(i,1);
+      }
+  }
+
+  res.send(tabledata);
 });
 
 app.listen(8088, function (err) {
